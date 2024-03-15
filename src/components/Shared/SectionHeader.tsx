fcf072e6d6186fe22e20d990e9a905cba550e433
Koushik <programmer.koushik@gmail.com>
2024-03-15 15:07:43 +0530

    animate

--- src/components/Shared/SectionHeader.tsx
+++ src/components/Shared/SectionHeader.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import { TbGridDots } from "react-icons/tb";
 
 type SectionHeaderProps = {
@@ -6,11 +9,18 @@ type SectionHeaderProps = {
 
 export default function SectionHeader({ children }: SectionHeaderProps) {
   return (
-    <div className="relative mb-[3.6rem]">
+    <motion.div
+      className="relative mb-[3.6rem]"
+      initial={{ opacity: 0, y: 80 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      // animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175, duration: 0.5 }}
+      viewport={{ once: true }}
+    >
       <TbGridDots className="absolute text-gray-600 text-[3rem] -z-10 -top-2 left-2" />
       <h2 className="text-[2.07rem] lg:text-[2.813rem] text-white font-medium ml-[1.8rem]">
         {children}
       </h2>
-    </div>
+    </motion.div>
   );
 }
