c3067582312bc1b58f71307c2ba9c193c9513550
Koushik <programmer.koushik@gmail.com>
2024-03-15 15:53:36 +0530

    update

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
   return (
     <div className="fixed z-30 xl:ml-[3rem] xl:mt-[1.3rem]">
       <div className="relative flex items-center mt-[0.3rem] ml-[0.7rem]">
-        <button className="ml-[0.3rem]" onClick={toggleLinks}>
+        <button className="ml-[0.4rem]" onClick={toggleLinks}>
           <p className="text-white text-[2rem] lg:text-[2.8rem] font-bold">
             K<span className="text-[#F74C60]">.</span>
           </p>
@@ -23,7 +23,7 @@ export default function NavBar() {
             {links.map((link, index) => (
               <li key={index} className="mb-[1.4rem]">
                 <button>
-                  <link.icon className="text-[#767599] text-[1.5rem] lgtext-[2rem] hover:text-[#D6B159] transition delay-75 duration-500 ease-in-out" />
+                  <link.icon className="text-[#767599] text-[1.5rem] lg:text-[2rem] hover:text-[#D6B159] transition delay-75 duration-500 ease-in-out" />
                 </button>
               </li>
             ))}
