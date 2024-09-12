8d68d6642af34caa5eec9a0f592bc62a0e84649f
Koushik Roy <120767897+Koushik1311@users.noreply.github.com>
2024-09-12 18:14:45 +0530

    Dev (#18)
    * remove
    
    * custom favicon
    
    * added nextjs toploader
    
    * NextTopLoader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inconsolata } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const overpass = Inconsolata({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body
         className={`${overpass.className} bg-[#282828] text-white flex flex-col justify-between h-screen`}
       >
+        <NextTopLoader color="#D28280" showSpinner={false} />
         {children}
 
         <Footer />
